61777c5e8f0b7081e97abc2c34e810368289e406
RaulSeguraIbanez <128618606+RaulSeguraIbanez@users.noreply.github.com>
2023-12-13 19:30:50 +0100

    a

--- Ejercicio1_factoriales/hilo.java
+++ Ejercicio1_factoriales/hilo.java
@@ -2,4 +2,13 @@ package Ejercicio1_factoriales;
 
 public class hilo extends Thread{
 	
+	private int numHilo;
+	
+	public hilo(int posicion) {
+		this.numHilo = posicion;
+	}
+	
+	public void run() {
+		
+	}
 }
--- Ejercicio1_factoriales/main.java
+++ Ejercicio1_factoriales/main.java
@@ -1,13 +1,22 @@
 package Ejercicio1_factoriales;
 import java.util.*;
 
+
 public class main {
 
 	public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
         
+        int numeroAFactorizar;
+        
         System.out.println("Ingrese la cantidad de números a factorizar:");
         int numProcesos = s1.nextInt();
         
+        hilo[] factorizaciones = new hilo[numProcesos];
+        
+        for (int i = 0; i < numProcesos; i++) {
+        	factorizaciones[i] = new hilo(i + 1);
+        	factorizaciones[i].start();
+        }
 	}
 }
