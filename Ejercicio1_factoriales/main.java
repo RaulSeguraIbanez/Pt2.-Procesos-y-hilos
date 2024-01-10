851a5f0dd523a3768acac051bb37527d941d2179
RaulSeguraIbanez <128618606+RaulSeguraIbanez@users.noreply.github.com>
2024-01-10 19:00:31 +0100

    Update main.java

--- Ejercicio1_factoriales/main.java
+++ Ejercicio1_factoriales/main.java
@@ -2,7 +2,7 @@ package Ejercicio1_factoriales;
 
 import java.util.Scanner;
 
-public class Main {
+public class main {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
